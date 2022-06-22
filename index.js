567ec2b070f2b29e082c8f3ab3a09ed9d723f8f9
Ayomimiketmm <aayomitan@gmail.com>
2022-06-22 09:33:49 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+//Addition
+add = 5 + 2
+console.log("Additon: " + add)
+
+//subtraction
+sub = 5 - 2
+console.log("Subtraction: " + sub)
+
+//multiplication
+mul = 5 * 2
+console.log("Multiplication: " + mul)
+
+//division
+div = 5 / 2
+console.log("Division: " + div)
